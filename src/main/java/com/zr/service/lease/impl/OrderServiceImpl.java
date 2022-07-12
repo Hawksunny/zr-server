850a1ba6af1c708339e62afd7eb3bce214f51ee3
_haks <1668938504@qq.com>
2022-07-12 22:53:34 +0800

    彻底完成了订单查询所用的后端接口

--- src/main/java/com/zr/service/lease/impl/OrderServiceImpl.java
+++ src/main/java/com/zr/service/lease/impl/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package com.zr.service.lease.impl;
 
+import com.github.pagehelper.PageHelper;
 import com.zr.mapper.lease.OrderMapper;
 import com.zr.service.lease.OrderService;
 import com.zr.vo.lease.OrderForOrderPage;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -13,8 +15,11 @@ public class OrderServiceImpl implements OrderService {
     @Resource
     private OrderMapper orderMapper;
 
+    @Transactional
     @Override
     public List<OrderForOrderPage> list(OrderForOrderPage order) {
+
+        PageHelper.startPage(order.getPageNum(), order.getPageSize());
         return orderMapper.list(order);
     }
 }
